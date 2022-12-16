cc82a458a9b5089a9780702ab640cfd6b74f6575
S <30459885+tacticstactics@users.noreply.github.com>
2022-12-16 13:57:09 +0900

    1

--- optical-heterodyne-detection/optical_heterodyne_detection.py
+++ optical-heterodyne-detection/optical_heterodyne_detection.py
@@ -1,24 +1,22 @@
 
 
 import numpy as np
-import math
-import cmath
 import matplotlib.pyplot as plt
 
 import mach_zender_interferometer_time_def
 
 print('')
-print('mach-zender_interferometer_time_main.py')
+print('optical-heterodyne-detection.py')
 print('')
 
 samplerate = 2048 # Sampling Frequency
 
 stept = 1/samplerate
 
-amp_c = 2.5
-freq_am = 5
+amp_c = 2*3.14
+freq_am = 2
 md = 1 # modulation depth. 1 = 100 %
-dc_offset = 2.1 # DC offset
+dc_offset = 0 # DC offset
 
 no = 1 # Refractive Index of medium
 
@@ -29,34 +27,27 @@ opl1 =100
 opl2= 100
 # Optical Path Length Difference (opl1-opl2) determines free spectral range as optical filter.
 
-wl = 0.633; #wavelength in um
+wl = 63.3; #wavelength
 
-PT1 = 0.5 # PT: Power Transmission of first beam splitter
-PT2 = 0.5 # PT: Power Transmission of second beam splitter
+PT1 = 0.5 # PT: Power Transmission of Beam splitter
 
 # Define Input Electric Field
 
-# Input Port 1 only
-Ein1 = np.array([[1+0.0000j],[0-0.0000j]])
-#Ein1 = np.array([[0.707+0.707j],[0]])
-
 # Input Both 1 and 2 port
 #Ein1 = np.array([[1+0j],[1-0j]]) 
-#Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]])
+Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]])
 #Ein1 = np.array([[1 + 0j],[-1 - 0j]])
-
-# Input Port 2 only
-#Ein1 = np.array([[0],[1]]) 
-#Ein1 = np.array([[0],[0.707+0.707j]])
 
 tcol = np.zeros(samplerate)
 signalcol = np.zeros(samplerate)
 
-P1_powercol = np.zeros(samplerate)
-P1_phasecol = np.zeros(samplerate)
+Port1_powercol = np.zeros(samplerate)
+Port1_EFcol = np.zeros(samplerate)
 
-P2_powercol = np.zeros(samplerate)
-P2_phasecol = np.zeros(samplerate)
+Port2_powercol = np.zeros(samplerate)
+Port2_EFcol = np.zeros(samplerate)
+
+Power_diffcol = np.zeros(samplerate)
 
 
 for ii in range(samplerate):
@@ -67,35 +58,30 @@ for ii in range(samplerate):
     signal = amp_c * np.sin(2 * np.pi * freq_am * t) + dc_offset
     signalcol[ii] = signal  
     
-    Eout1 = mach_zender_interferometer_time_def.propagate1(wl, no, oplcommon1, oplcommon2, Ein1)
+    Eout1 = mach_zender_interferometer_time_def.propagate1(wl, no, opl1, opl2+signal, Ein1)
     Ein2 = Eout1
     
     Eout2 = mach_zender_interferometer_time_def.beamsplitter(PT1, Ein2)
-    Ein3 = Eout2
+    Ein3 = Eout2    
+   
+    Eout3 = mach_zender_interferometer_time_def.propagate1(wl, no, oplcommon1, oplcommon2, Ein3)
     
-    Eout3 = mach_zender_interferometer_time_def.propagate1(wl, no, opl1, opl2+signal, Ein3) # Each path experience different path length
-    Ein4 = Eout3
+    Port1_Eout = Eout3[0,0] # Trans
+
+    Port1_EFcol[ii] = Port1_Eout
+
+    power_11 = (np.abs(Port1_Eout))**2 # Optical power is calculated as square of absolute electric field strength
+    Port1_powercol[ii] = power_11
     
-    Eout4 = mach_zender_interferometer_time_def.beamsplitter(PT2, Ein4) # Each path enter second beam splitter
-    Ein5 = Eout4
     
-    Eout5 = mach_zender_interferometer_time_def.propagate1(wl, no, oplcommon1, oplcommon2, Ein5)
-    Ein6 = Eout5
+    Port2_Eout = Eout3[1,0] #Reflect
+    power_22 = (np.abs(Port2_Eout))**2
     
-    Eout_port1 = Ein6[0,0] 
-    power_11 = (np.abs(Eout_port1))**2 # Optical power is calculated as square of absolute electric field strength
-    P1_powercol[ii] = power_11
-    
-    P1_phase = cmath.phase(power_11)
-    P1_phasecol[ii] = P1_phase
-    
-    Eout_port_2 = Ein6[1,0]
-    power_22 = (np.abs(Eout_port_2))**2
-    
-    P2_powercol[ii] = power_22
-    
-    P2_phase = cmath.phase(power_22)
-    P2_phasecol[ii] = P2_phase
+    Port2_powercol[ii] = power_22
+    Port2_EFcol[ii] = Port2_Eout
+
+    Power_diff = power_11 - power_22
+    Power_diffcol[ii] = Power_diff
  
  
 
@@ -108,16 +94,16 @@ ax3 = fig.add_subplot(3, 1, 3)
 ax1.plot(tcol,signalcol)
 #ax1.set_ylim(-3,3)
 
-ax2.plot(tcol,P1_powercol,tcol,P2_powercol)
+ax2.plot(tcol,Port1_powercol,tcol,Port2_powercol)
 ax2.set_xlabel("time")
 ax2.set_ylabel("Power")
-ax2.set_ylim(0,1.1)
+ax2.set_ylim(0,2.1)
 ax2.grid()
 
-ax3.plot(tcol,P1_phasecol,tcol,P2_phasecol)
+ax3.plot(tcol,Power_diffcol)
 ax3.set_xlabel("time")
-ax3.set_ylabel("Phase")
-ax3.set_ylim(-2,2)
+ax3.set_ylabel("Power Difference")
+#ax3.set_ylim(-2,2)
 ax3.grid()
 
 plt.show()
